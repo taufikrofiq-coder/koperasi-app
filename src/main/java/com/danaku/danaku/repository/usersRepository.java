daf2535bc8fe447b100d0151dc4289f9817242b0
taufikrofiq-coder <taufikrofiq@gmail.com>
2026-01-29 21:11:04 +0700

    update

--- src/main/java/com/danaku/danaku/repository/usersRepository.java
+++ src/main/java/com/danaku/danaku/repository/usersRepository.java
@@ -1,4 +1,5 @@
 package com.danaku.danaku.repository;
+
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,8 +9,8 @@ import com.danaku.danaku.entity.usersEntity;
 
 @Repository
 public interface usersRepository extends JpaRepository<usersEntity, Long> {
+    // Perhatikan: Generic type Long sesuai dengan userId tipe Long
     boolean existsByUserName(String userName);
-    Optional<usersEntity> findByNoRekening(String noRekening); // Pastikan ada method ini
-    Optional<usersEntity> findByUserName(String userName); // Optional: jika diperlukan
+    Optional<usersEntity> findByNoRekening(String noRekening);
+    Optional<usersEntity> findByUserName(String userName);
 }
-     
